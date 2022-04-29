9863506a7a8c9dc81f4d1e782031b197e89ffacb
winstonwang <wwang@lcs.on.ca>
2022-04-29 14:46:28 -0400

    image is not filled before tap and is filled after tap

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -11,35 +11,56 @@ import UIKit
 
 struct CustomComposableView: View {
     //MARK: computed properties
-    @State var currentColor: Color = .blue
-
+    @State var currentColor: Color = .black
     @State var xOffset: CGFloat = 0.0
-    
-    @State private var size: CGFloat = 200.00
+    @State var scaleFactor = 1.0
+    @State var fillImageOpacity = 0.0
+    @State var unfillImageOpacity = 1.0
     var body: some View {
-        VStack{
+        ZStack{
+            
+            // filled image
             Image(systemName: "x.circle.fill")
                 .resizable()
                 .frame(width: 60, height: 60)
                 .offset(x: xOffset, y: 0)
+                .scaleEffect(scaleFactor)
                 .foregroundColor(currentColor)
             
-            
-                
                 .onTapGesture {
                     withAnimation(
-                     Animation
-                        .interpolatingSpring(stiffness: 1000, damping: 0.5)
-                     
+                        Animation
+                            .interpolatingSpring(stiffness: 2000, damping: 6)
                     ) {
                         xOffset = 5
-                        size = 100
+                        unfillImageOpacity = 1.0
+                        fillImageOpacity = 0.0
                     }
                     currentColor = .red
+                    scaleFactor = 2.0
                     
-
                 }
-            
+            // unfilled image
+            Image(systemName: "x.circle")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .offset(x: xOffset, y: 0)
+                .scaleEffect(scaleFactor)
+                .foregroundColor(currentColor)
+                .opacity(unfillImageOpacity)
+            onTapGesture {
+                withAnimation(
+                    Animation
+                        .interpolatingSpring(stiffness: 2000, damping: 6)
+                ) {
+                    xOffset = 5
+                    unfillImageOpacity = 0.0
+                    fillImageOpacity = 1.0
+                }
+                currentColor = .red
+                scaleFactor = 2.0
+                
+            }
         }
     }
 }
